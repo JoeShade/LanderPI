385b984a4078b09b4e3881671bfe58295e44e416
joeShade <joe.shade@outlook.com>
2025-12-03 10:39:31 +0000

    Created setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from glob import glob
+from setuptools import setup
+
+package_name = 'green_nav_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+    ],
+    description='Green line following node and launches',
+    entry_points={
+        'console_scripts': [
+            'green_nav = green_nav:main'
+        ],
+    },
+)
